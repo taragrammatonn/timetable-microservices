cd30fcab9c753213a7b4b3f9d928db36004ffd3a
Jessus <liubomir75@gmail.com>
2021-01-22 22:50:03 +0200

    changelog update

--- db-service/src/main/resources/db/changelog/changelog-1.0.postgresql.sql
+++ db-service/src/main/resources/db/changelog/changelog-1.0.postgresql.sql
@@ -1,7 +1,7 @@
 create schema if not exists parsing;
 create schema if not exists users;
 
-create table if not exists audience
+create table if not exists parsing.audience
 (
     id   bigint not null
         constraint audience_pkey
@@ -9,10 +9,24 @@ create table if not exists audience
     name varchar(255)
 );
 
-alter table audience
+alter table parsing.audience
     owner to postgres;
 
-create table if not exists "group"
+create table if not exists parsing.daily_parameters
+(
+    id              bigint       not null
+        constraint daily_parameters_pkey
+            primary key,
+    day             varchar(255),
+    parameters_date varchar(255) not null,
+    semester        varchar(255),
+    week            varchar(255)
+);
+
+alter table parsing.daily_parameters
+    owner to postgres;
+
+create table if not exists parsing."group"
 (
     id   bigint not null
         constraint group_pkey
@@ -20,10 +34,10 @@ create table if not exists "group"
     name varchar(255)
 );
 
-alter table "group"
+alter table parsing."group"
     owner to postgres;
 
-create table if not exists lesson
+create table if not exists parsing.lesson
 (
     id             bigint not null
         constraint lesson_pkey
@@ -37,10 +51,10 @@ create table if not exists lesson
     teacher_name   varchar(255)
 );
 
-alter table lesson
+alter table parsing.lesson
     owner to postgres;
 
-create table if not exists teacher
+create table if not exists parsing.teacher
 (
     id   bigint not null
         constraint teacher_pkey
@@ -48,33 +62,19 @@ create table if not exists teacher
     name varchar(255)
 );
 
-alter table teacher
+alter table parsing.teacher
     owner to postgres;
 
-create table if not exists daily_parameters
-(
-    id              bigint  not null
-        constraint daily_parameters_pkey
-            primary key,
-    day             varchar(255),
-    parameters_date varchar not null,
-    semester        varchar(255),
-    week            varchar(255)
-);
-
-alter table daily_parameters
-    owner to postgres;
-
-
-
-create table if not exists "user"
+create table if not exists users."user"
 (
     id             bigint not null
         constraint user_pkey
             primary key,
     active         boolean,
     admin_entity   boolean,
-    chat_id        bigint,
+    chat_id        bigint
+        constraint uk_2x9mn7ijqx1uaori01f3w9hoj
+            unique,
     f_name         varchar(255),
     l_name         varchar(255),
     user_group     varchar(255),
@@ -82,21 +82,26 @@ create table if not exists "user"
     user_nick_name varchar(255)
 );
 
-alter table "user"
+alter table users."user"
     owner to postgres;
-
-create schema if not exists history_data;
 
 create table if not exists history_data.history
 (
-    id      bigint not null
+    id              bigint not null
         constraint history_pkey
             primary key,
-    event   varchar(255),
-    user_id bigint
+    event           varchar(255),
+    user_id         bigint
         constraint fkn4gjyu69m6xa5f3bot571imbe
+            references users."user",
+    request_date    varchar(255),
+    request_message varchar(255),
+    user_chat_id    bigint,
+    chat_id         bigint
+        constraint fk4prakw3qr3295fc83u2xht98n
             references users."user"
 );
 
 alter table history_data.history
     owner to postgres;
+
