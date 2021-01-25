e79bbbb51b98e72ba75abea82c6db731335ba525
Jessus <liubomir75@gmail.com>
2021-01-25 21:36:48 +0200

    update changelog

--- db-service/src/main/resources/db/changelog/changelog-1.0.postgresql.sql
+++ db-service/src/main/resources/db/changelog/changelog-1.0.postgresql.sql
@@ -3,24 +3,24 @@ create schema if not exists users;
 
 create table if not exists parsing.audience
 (
-    id   bigint not null
-        constraint audience_pkey
-            primary key,
-    name varchar(255)
+    id   bigint not null,
+    name varchar(255),
+    constraint audience_pkey
+        primary key (id)
 );
 
-alter table parsing.audience
+alter table audience
     owner to postgres;
 
 create table if not exists parsing.daily_parameters
 (
-    id              bigint       not null
-        constraint daily_parameters_pkey
-            primary key,
+    id              bigint       not null,
     day             varchar(255),
     parameters_date varchar(255) not null,
     semester        varchar(255),
-    week            varchar(255)
+    week            varchar(255),
+    constraint daily_parameters_pkey
+        primary key (id)
 );
 
 alter table parsing.daily_parameters
@@ -28,10 +28,10 @@ alter table parsing.daily_parameters
 
 create table if not exists parsing."group"
 (
-    id   bigint not null
-        constraint group_pkey
-            primary key,
-    name varchar(255)
+    id   bigint not null,
+    name varchar(255),
+    constraint group_pkey
+        primary key (id)
 );
 
 alter table parsing."group"
@@ -39,16 +39,16 @@ alter table parsing."group"
 
 create table if not exists parsing.lesson
 (
-    id             bigint not null
-        constraint lesson_pkey
-            primary key,
+    id             bigint not null,
     cours_name     varchar(255),
     cours_office   varchar(255),
     day_number     integer,
     group_name     varchar(255),
     lesson_number  varchar(255),
     sub_group_name varchar(255),
-    teacher_name   varchar(255)
+    teacher_name   varchar(255),
+    constraint lesson_pkey
+        primary key (id)
 );
 
 alter table parsing.lesson
@@ -56,30 +56,32 @@ alter table parsing.lesson
 
 create table if not exists parsing.teacher
 (
-    id   bigint not null
-        constraint teacher_pkey
-            primary key,
-    name varchar(255)
+    id   bigint not null,
+    name varchar(255),
+    constraint teacher_pkey
+        primary key (id)
 );
 
 alter table parsing.teacher
     owner to postgres;
 
+
+
 create table if not exists users."user"
 (
-    id             bigint not null
-        constraint user_pkey
-            primary key,
+    id             bigint not null,
     active         boolean,
     admin_entity   boolean,
-    chat_id        bigint
-        constraint uk_2x9mn7ijqx1uaori01f3w9hoj
-            unique,
+    chat_id        bigint,
     f_name         varchar(255),
     l_name         varchar(255),
     user_group     varchar(255),
     user_language  varchar(255),
-    user_nick_name varchar(255)
+    user_nick_name varchar(255),
+    constraint user_pkey
+        primary key (id),
+    constraint uk_2x9mn7ijqx1uaori01f3w9hoj
+        unique (chat_id)
 );
 
 alter table users."user"
@@ -87,19 +89,19 @@ alter table users."user"
 
 create table if not exists history_data.history
 (
-    id              bigint not null
-        constraint history_pkey
-            primary key,
+    id              bigint not null,
     event           varchar(255),
-    user_id         bigint
-        constraint fkn4gjyu69m6xa5f3bot571imbe
-            references users."user",
+    user_id         bigint,
     request_date    varchar(255),
     request_message varchar(255),
     user_chat_id    bigint,
-    chat_id         bigint
-        constraint fk4prakw3qr3295fc83u2xht98n
-            references users."user"
+    chat_id         bigint,
+    constraint history_pkey
+        primary key (id),
+    constraint fkn4gjyu69m6xa5f3bot571imbe
+        foreign key (user_id) references users."user",
+    constraint fk4prakw3qr3295fc83u2xht98n
+        foreign key (chat_id) references users."user"
 );
 
 alter table history_data.history
--- db-service/src/main/resources/db/changelog/changelog.postgresql.sql
+++ db-service/src/main/resources/db/changelog/changelog.postgresql.sql
@@ -1,38 +1,40 @@
 --liquibase formatted sql
 
---changeset Jessus:1611212706075-1
+--changeset Jessus:1611603191359-1
 CREATE SEQUENCE hibernate_sequence;
 
---changeset Jessus:1611212706075-2
+--changeset Jessus:1611603191359-2
 CREATE TABLE audience (id BIGINT NOT NULL, name VARCHAR(255), CONSTRAINT audience_pkey PRIMARY KEY (id));
 
---changeset Jessus:1611212706075-3
+--changeset Jessus:1611603191359-3
 CREATE TABLE daily_parameters (id BIGINT NOT NULL, day VARCHAR(255), parameters_date VARCHAR NOT NULL, semester VARCHAR(255), week VARCHAR(255), CONSTRAINT daily_parameters_pkey PRIMARY KEY (id));
 
---changeset Jessus:1611212706075-4
+--changeset Jessus:1611603191359-4
 CREATE TABLE "group" (id BIGINT NOT NULL, name VARCHAR(255), CONSTRAINT group_pkey PRIMARY KEY (id));
 
---changeset Jessus:1611212706075-5
+--changeset Jessus:1611603191359-5
 CREATE TABLE hibernate_sequences (sequence_name VARCHAR(255) NOT NULL, next_val BIGINT, CONSTRAINT hibernate_sequences_pkey PRIMARY KEY (sequence_name));
 
---changeset Jessus:1611212706075-6
+--changeset Jessus:1611603191359-6
 CREATE TABLE lesson (id BIGINT NOT NULL, cours_name VARCHAR(255), cours_office VARCHAR(255), day_number INTEGER, group_name VARCHAR(255), lesson_number VARCHAR(255), sub_group_name VARCHAR(255), teacher_name VARCHAR(255), CONSTRAINT lesson_pkey PRIMARY KEY (id));
 
---changeset Jessus:1611212706075-7
+--changeset Jessus:1611603191359-7
 CREATE TABLE teacher (id BIGINT NOT NULL, name VARCHAR(255), CONSTRAINT teacher_pkey PRIMARY KEY (id));
 
---changeset Jessus:1611212706075-8
+--changeset Jessus:1611603191359-8
 CREATE TABLE "user" (id BIGINT NOT NULL, active BOOLEAN, admin_entity BOOLEAN, chat_id BIGINT, f_name VARCHAR(255), l_name VARCHAR(255), user_group VARCHAR(255), user_language VARCHAR(255), user_nick_name VARCHAR(255), CONSTRAINT user_pkey PRIMARY KEY (id));
 
---changeset Jessus:1611212706075-9
+--changeset Jessus:1611603191359-9
 CREATE TABLE user_history (user_id BIGINT NOT NULL, history_id BIGINT NOT NULL);
 
---changeset Jessus:1611212706075-10
+--changeset Jessus:1611603191359-10
 ALTER TABLE user_history ADD CONSTRAINT uk_qs60mt4ff67ydg2sdxln0uu68 UNIQUE (history_id);
 
---changeset Jessus:1611212706075-11
+--changeset Jessus:1611603191359-11
 ALTER TABLE user_history ADD CONSTRAINT fkaa6ilb6iqih95bntoeyysb2pc FOREIGN KEY (user_id) REFERENCES users."user" (id) ON UPDATE NO ACTION ON DELETE NO ACTION;
 
---changeset Jessus:1611212706075-12
+--changeset Jessus:1611603191359-12
 ALTER TABLE user_history ADD CONSTRAINT fkmyn0e5qi70qvka9b47oaedacw FOREIGN KEY (history_id) REFERENCES history_data.history (id) ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+ATE NO ACTION ON DELETE NO ACTION;
 
