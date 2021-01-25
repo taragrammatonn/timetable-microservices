9eb35874f0a98c614aadd0df1c8bcc929eb63c40
Jessus <liubomir75@gmail.com>
2021-01-25 21:55:07 +0200

    add History exception

---
+++ telegram-user-service/src/main/java/com/flux/telegramservice/util/exception/CannotSaveHistoryException.java
@@ -0,0 +1,8 @@
+package com.flux.telegramservice.util.exception;
+
+public class CannotSaveHistoryException extends Exception {
+
+    public CannotSaveHistoryException(String message) {
+        super(message);
+    }
+}
