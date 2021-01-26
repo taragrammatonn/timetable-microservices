b03ec5651d0db30ce9f11d8ecc924182d2a2da83
Jessus <liubomir75@gmail.com>
2021-01-26 21:38:29 +0200

    add todo

---
+++ telegram-user-service/src/test/java/com/flux/telegramservice/service/project/BotServiceTest.java
@@ -0,0 +1,13 @@
+package com.flux.telegramservice.service.project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+
+class BotServiceTest {
+
+    @Test
+    void findLessonsByGroup() {
+    }
+}
