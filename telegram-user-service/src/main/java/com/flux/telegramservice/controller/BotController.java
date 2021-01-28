85b75d1e11ff324762ca96f6eebbb1dae6b6d082
Jessus <liubomir75@gmail.com>
2021-01-28 20:57:37 +0200

    hand of help

--- telegram-user-service/src/main/java/com/flux/telegramservice/controller/BotController.java
+++ telegram-user-service/src/main/java/com/flux/telegramservice/controller/BotController.java
@@ -8,6 +8,8 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import static java.util.Objects.isNull;
+
 @Component
 @Slf4j
 public class BotController extends Bot {
@@ -16,15 +18,33 @@ public class BotController extends Bot {
     @Override
     public void onUpdateReceived(Update update) {
 
-        switch (update.getMessage().getText()) {
+        switch (messageProcessing(update)) {
             case "/start" -> {
                 sendMessage(update, userService.completeUser(userService.addNewUser(update)));
                 execute(setButtons(update.getMessage().getChatId()));
             }
+
+            case "ALPHA" -> {
+                execute(new SendMessage().setChatId(getChatId(update)).setText(MY_PINUS));
+            }
+
+            case "Misha" -> {
+                execute(new SendMessage().setChatId(getChatId(update)).setText(MISHA_PENIS));
+            }
+
             default -> {
-                sendPenis(update.getCallbackQuery().getData(), update.getMessage().getChatId());
                 sendMessage(update, botService.findLessonsByGroup(update));
             }
         }
+    }
+
+    private String messageProcessing(Update update) {
+        if (!isNull(update.getMessage()))
+            return update.getMessage().getText();
+        else return update.getCallbackQuery().getData();
+    }
+
+    private Long getChatId(Update update) {
+        return update.getMessage() != null ? update.getMessage().getChatId() : update.getCallbackQuery().getMessage().getChatId();
     }
 }
