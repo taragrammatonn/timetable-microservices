9e19af37037827ebca6dd957cc014745353fab24
Mihai Cojusnean <mihai.cojusnean@gmail.com>
2021-01-28 19:31:12 +0100

    set Buttons

--- telegram-user-service/src/main/java/com/flux/telegramservice/botconfiguration/Bot.java
+++ telegram-user-service/src/main/java/com/flux/telegramservice/botconfiguration/Bot.java
@@ -5,7 +5,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 public abstract class Bot extends TelegramLongPollingBot {
@@ -27,6 +36,31 @@ public abstract class Bot extends TelegramLongPollingBot {
         log.info("\n###################--> Message send to: " + update.getMessage().getChat().getFirstName() + " " + update.getMessage().getChat().getLastName() + " <-- ###################\n" +
                 "###################--> Message text   : " + update.getMessage().getText() + "               <-- ###################");
         execute(new SendMessage().enableMarkdown(true).setChatId(update.getMessage().getChatId()).setText(message));
+    }
+
+    public SendMessage setButtons(long chatId) {
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
+        inlineKeyboardButton1.setText("Liubomir");
+        inlineKeyboardButton1.setCallbackData("Button \"Liubomir\" has been pressed");
+        List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
+        List<InlineKeyboardButton> keyboardButtonsRow2 = new ArrayList<>();
+        keyboardButtonsRow1.add(inlineKeyboardButton1);
+        keyboardButtonsRow1.add(new InlineKeyboardButton().setText("Misha").setCallbackData("Misha"));
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+        rowList.add(keyboardButtonsRow1);
+        rowList.add(keyboardButtonsRow2);
+        inlineKeyboardMarkup.setKeyboard(rowList);
+        return new SendMessage().setChatId(chatId).setText("Показать размер пениса").setReplyMarkup(inlineKeyboardMarkup);
+    }
+
+    public void sendPenis(String a, long chatId) {
+        switch (a) {
+            case "Misha":
+                new SendMessage().setChatId(chatId).setText("8============================D");
+            case "Liubomir":
+                new SendMessage().setChatId(chatId).setText("8=D");
+        }
     }
 
     @Override
--- telegram-user-service/src/main/java/com/flux/telegramservice/controller/BotController.java
+++ telegram-user-service/src/main/java/com/flux/telegramservice/controller/BotController.java
@@ -1,51 +1,30 @@
 package com.flux.telegramservice.controller;
 
 import com.flux.telegramservice.botconfiguration.Bot;
-import com.flux.telegramservice.controller.generator.CommandGenerator;
-import com.flux.telegramservice.service.project.BotService;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static java.util.Objects.isNull;
 
 @Component
 @Slf4j
 public class BotController extends Bot {
 
-    private final Map<String, CommandGenerator> commands = new HashMap<>();
-
-    @Autowired
-    private BotService botService;
-
     @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
-        send(update);
-    }
 
-    public void send(Update update) {
-        String command = update.getMessage().getText();
-
-        CommandGenerator commandGenerator = commands.get(command);
-
-        if (commandGenerator == null) {
-            String response = botService.searchCommand(command, update);
-
-            if (isNull(response)) {
-                throw new UnsupportedOperationException("Command \"" + command + "\" not supported yet.");
+        switch (update.getMessage().getText()) {
+            case "/start" -> {
+                sendMessage(update, userService.completeUser(userService.addNewUser(update)));
+                execute(setButtons(update.getMessage().getChatId()));
             }
-
-            sendMessage(update, response);
-        } else sendMessage(update, commandGenerator.generateCommand(update));
-    }
-
-    public void register(String code, CommandGenerator generator) {
-        commands.put(code, generator);
+            default -> {
+                sendPenis(update.getCallbackQuery().getData(), update.getMessage().getChatId());
+                sendMessage(update, botService.findLessonsByGroup(update));
+            }
+        }
     }
 }
