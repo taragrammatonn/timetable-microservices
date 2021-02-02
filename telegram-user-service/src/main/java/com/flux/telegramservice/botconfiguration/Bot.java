2455c5bf15e3c56f97a628bf8e37d3a6dd43a2e3
Mihai Cojusnean <mihai.cojusnean@gmail.com>
2021-02-02 17:20:17 +0100

    add buttons

--- telegram-user-service/src/main/java/com/flux/telegramservice/botconfiguration/Bot.java
+++ telegram-user-service/src/main/java/com/flux/telegramservice/botconfiguration/Bot.java
@@ -7,7 +7,10 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -51,6 +54,21 @@ public abstract class Bot extends TelegramLongPollingBot {
         rowList.add(keyboardButtonsRow2);
         inlineKeyboardMarkup.setKeyboard(rowList);
         return new SendMessage().setChatId(chatId).setText("Показать размер пениса").setReplyMarkup(inlineKeyboardMarkup);
+    }
+
+    public void setStickyButtons(long chatId) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+        List<KeyboardRow> keyboard = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        keyboardFirstRow.add(new KeyboardButton("Grupa"));
+        keyboardFirstRow.add(new KeyboardButton("Profesor"));
+        keyboardFirstRow.add(new KeyboardButton("Hata"));
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+        keyboard.add(keyboardFirstRow);
+        replyKeyboardMarkup.setKeyboard(keyboard);
     }
 
     @Override
