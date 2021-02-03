ab3255de579a2004ad31ce69afb14b78d99103e2
Jessus <liubomir75@gmail.com>
2021-02-03 23:00:36 +0200

    hot fix then started 2 or more instances of poling bot

--- telegram-user-service/src/main/java/com/flux/telegramservice/controller/generator/impl/StartMessageGenerator.java
+++ telegram-user-service/src/main/java/com/flux/telegramservice/controller/generator/impl/StartMessageGenerator.java
@@ -1,6 +1,5 @@
 package com.flux.telegramservice.controller.generator.impl;
 
-import com.flux.telegramservice.botconfiguration.Bot;
 import com.flux.telegramservice.controller.generator.CommandGenerator;
 import com.flux.telegramservice.service.project.UserService;
 import org.springframework.stereotype.Component;
@@ -14,32 +13,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class StartMessageGenerator extends Bot implements CommandGenerator {
+public class StartMessageGenerator implements CommandGenerator {
 
     protected final UserService userService;
-    protected final SendMessage sendMessage;
 
-    public StartMessageGenerator(UserService userService, SendMessage sendMessage) {
+    public StartMessageGenerator(UserService userService) {
         this.userService = userService;
-        this.sendMessage = sendMessage;
     }
 
     @Override
-    public void generateCommand(Update update) {
-        sendMessage(update, userService.completeUser(userService.addNewUser(update)));
+    public SendMessage generateCommand(Update update) {
+        return new SendMessage(
+                update.getMessage().getChatId(), userService.completeUser(userService.addNewUser(update))
+        ).enableMarkdown(true).setReplyMarkup(setStickyButtons());
     }
 
     @Override
     public String getInputCommand() {
         return "/start";
-    }
-
-    @Override
-    protected void sendMessage(Update update, String message) {
-        sendMessage.setChatId(update.getMessage().getChatId());
-        sendMessage.enableMarkdown(true);
-        sendMessage.setText(message);
-        sendMessage.setReplyMarkup(setStickyButtons());
     }
 
     public ReplyKeyboardMarkup setStickyButtons() {
