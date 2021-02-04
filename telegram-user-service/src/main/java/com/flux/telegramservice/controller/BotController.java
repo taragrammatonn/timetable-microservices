fcafb79491c7b680182409c519a31afe185dd009
Jessus <liubomir75@gmail.com>
2021-02-04 13:13:49 +0200

    add logic for adding days

--- telegram-user-service/src/main/java/com/flux/telegramservice/controller/BotController.java
+++ telegram-user-service/src/main/java/com/flux/telegramservice/controller/BotController.java
@@ -2,10 +2,12 @@ package com.flux.telegramservice.controller;
 
 import com.flux.telegramservice.botconfiguration.Bot;
 import com.flux.telegramservice.controller.generator.CommandGenerator;
+import com.flux.telegramservice.controller.generator.impl.AddDaysCommandGenerator;
 import com.flux.telegramservice.service.project.BotService;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.util.HashMap;
@@ -33,19 +35,41 @@ public class BotController extends Bot {
 
     @SneakyThrows
     public void send(Update update) {
-        String command = update.getMessage().getText();
+        String command;
 
-        CommandGenerator commandGenerator = commands.get(command);
+        if (update.getMessage() != null) {
+            command = update.getMessage().getText();
+            CommandGenerator commandGenerator = commands.get(command);
 
-        if (commandGenerator == null) {
-            String response = botService.searchCommand(command, update);
+            if (commandGenerator == null) {
+                String response = botService.searchCommand(command, update);
 
-            if (isNull(response)) {
-                throw new UnsupportedOperationException("Command \"" + command + "\" not supported yet.");
+                if (isNull(response)) {
+                    throw new UnsupportedOperationException("Command \"" + command + "\" not supported yet.");
+                }
+
+                sendMessage(update, response);
+            } else execute(commandGenerator.generateCommand(update));
+        } else {
+            command = update.getCallbackQuery().getData();
+
+            AddDaysCommandGenerator addDaysCommandGenerator = new AddDaysCommandGenerator() {
+
+                @Override
+                public SendMessage generateCommand(Update update) {
+                    return super.generateCommand(update);
+                }
+
+                @Override
+                public String getInputCommand() {
+                    return command;
+                }
+            };
+
+            if (addDaysCommandGenerator.getCommandsList().contains(command)) {
+                addDaysCommandGenerator.generateCommand(update);
             }
-
-            sendMessage(update, response);
-        } else execute(commandGenerator.generateCommand(update));
+        }
     }
 
     public void register(String code, CommandGenerator generator) {
