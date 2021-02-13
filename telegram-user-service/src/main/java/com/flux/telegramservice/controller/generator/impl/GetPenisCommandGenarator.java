90aa4f2085282b10625a29325976e187e9e733b6
Jessus <liubomir75@gmail.com>
2021-02-13 21:18:48 +0200

    after review

--- telegram-user-service/src/main/java/com/flux/telegramservice/controller/generator/impl/GetPenisCommandGenarator.java
+++ telegram-user-service/src/main/java/com/flux/telegramservice/controller/generator/impl/GetPenisCommandGenarator.java
@@ -4,29 +4,16 @@ import com.flux.telegramservice.controller.generator.CommandGenerator;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.security.SecureRandom;
 
 @Component
 public class GetPenisCommandGenarator implements CommandGenerator {
 
     @Override
     public SendMessage generateCommand(Update update) {
-        String response = "Чей апарат желаете увидеть";
-        return new SendMessage(update.getMessage().getChatId(), response).setReplyMarkup(setButtons());
-    }
-
-    public InlineKeyboardMarkup setButtons() {
-        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
-        List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
-        keyboardButtonsRow1.add(new InlineKeyboardButton().setText("Misha").setCallbackData("Misha"));
-        keyboardButtonsRow1.add(new InlineKeyboardButton().setText("Liubomir").setCallbackData("Liubomir"));
-        inlineKeyboardMarkup.setKeyboard(Collections.singletonList(keyboardButtonsRow1));
-        return inlineKeyboardMarkup;
+        String response = "8" + "=".repeat(Math.max(0, (new SecureRandom().nextInt(30)))) + "D";
+        return new SendMessage(update.getMessage().getChatId(), response);
     }
 
     @Override
